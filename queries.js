a87f2a6a03c6062c7f0a1f6edc5cb2dc3d3a527e
collinsbj <collinsbj@me.com>
2018-02-06 11:59:15 -0700

    updated update query -BJ

--- queries.js
+++ queries.js
@@ -1,31 +1,31 @@
 const database = require("./database-connection");
 
-      module.exports = {
-        list(db) {
-          return database(db).select();
-        },
-        read(db, id) {
-          return database(db)
-            .select()
-            .where("id", id)
-            .first();
-        },
-        create(db , body) {
-          return database(db)
-            .insert(body)
-            .returning("*")
-            .then(record => record[0]);
-        },
-        update(database, id, resolution) {
-          return database(db)
-            .update(resolution)
-            .where("id", id)
-            .returning("*")
-            .then(record => record);
-        },
-        delete(db, id) {
-          return database(db)
-            .delete()
-            .where("id", id);
-        }
-    };
+module.exports = {
+  list(db) {
+    return database(db).select();
+  },
+  read(db, id) {
+    return database(db)
+      .select()
+      .where("id", id)
+      .first();
+  },
+  create(db, body) {
+    return database(db)
+      .insert(body)
+      .returning("*")
+      .then(record => record[0]);
+  },
+  update(database, id, resolution) {
+    return database(database)
+      .update(resolution)
+      .where("id", id)
+      .returning("*")
+      .then(record => record);
+  },
+  delete(db, id) {
+    return database(db)
+      .delete()
+      .where("id", id);
+  }
+};
